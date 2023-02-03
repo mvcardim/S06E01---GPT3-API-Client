b005d16dd2eab4ce60d7b46c96e201380bbe08bc
marco cardim <mvcardim@hotmail.com>
2023-02-03 17:28:06 -0200

    salvando codigo ok

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ if (args.Length > 0)
 {
     HttpClient client = new HttpClient();
 
-    client.DefaultRequestHeaders.Add("authorization", "Bearer <INSERT YOUR API KEY HERE>");
+    client.DefaultRequestHeaders.Add("authorization", "Bearer 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");
 
     var content = new StringContent("{\"model\": \"text-davinci-001\", \"prompt\": \""+ args[0] +"\",\"temperature\": 1,\"max_tokens\": 100}",
         Encoding.UTF8, "application/json");
